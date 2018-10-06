3b786896c4386403dbedbc00a7ed846610c7e24c
Nestor Velez <nvelez64@yahoo.com>
2018-10-06 14:28:17 -0400

    html update

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 let foo = $.get('http://api.tripadvisor.com/api/partner/2.0/map/42.33141,-71.099396?key=ed13fbcb-dab4-41ff-b0d1-6f0528a99f4d', (err, resp) => {
   console.log(err, resp);
 });
+
+/*                                                         */
+
+getLocation();
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+    "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+      x.innerHTML = console.log(position.coords.latitude);
+     console.log(position.coords.longitude);
+    }
